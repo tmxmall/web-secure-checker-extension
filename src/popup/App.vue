e3bdc5e7be95aafa9fcc856956e60d1a6ded75b6
Chen Yuncai <chenyuncai@tmxmall.com>
2021-12-28 11:34:24 +0800

    init

---
+++ src/popup/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="popup-page">
+    hi. i am popup page
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Welcome!',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.popup-page {
+  min-height: 200px;
+  min-width: 400px;
+  color: gray;
+}
+
+</style>
